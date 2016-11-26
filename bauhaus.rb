7df718eb2a8eabe723c5a71ed573971d87790b0f
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2016-11-26 16:42:53 +0100

    initial

---
+++ bauhaus.rb
@@ -0,0 +1,21 @@
+class Bauhaus
+  def double_dare
+    i_dare_you_to_be_real
+    to_touch_a_flickering_flame
+    the_pangs_of_dark_delight
+    dont_cower_in_night_fright
+
+    dont_back_away_just_yet
+    from_destinations_set
+    i_dare_you_to_be_proud
+    to_dare_to_shout_aloud
+    for_convictions_that_you_feel
+    like_sound_from_bells_to_peal
+    i_dare_you_to_speak_of_your_despise
+    for_bureacracy_hypocrysy_all_liars
+
+    i_dare
+    i_dare
+    i_dare_you_you
+  end
+end
